c33474a376f5796d10718a3e56aef0d2f8e9496a
al3xandr3-bruno <brunoalexdias15@gmail.com>
2025-05-09 22:32:14 -0300

    ✨ Find an Engine by it's name

--- src/main/java/com/gamershall/domain/repository/EngineRepository.java
+++ src/main/java/com/gamershall/domain/repository/EngineRepository.java
@@ -3,5 +3,9 @@ package com.gamershall.domain.repository;
 import com.gamershall.domain.model.Engine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EngineRepository extends JpaRepository<Engine, Long> {
+
+    Optional<Engine> findByNome(String Nome);
 }
