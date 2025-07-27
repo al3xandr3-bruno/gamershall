a0571d64cc77e0d2866d427d1246cfe6a1e47cc5
al3xandr3-bruno <brunoalexdias15@gmail.com>
2025-07-26 23:14:02 -0300

    ✏️ Fixing user's played games service class

--- src/main/java/com/gamershall/domain/services/RegistroJogoJogadoService.java
+++ src/main/java/com/gamershall/domain/services/RegistroJogoJogadoService.java
@@ -5,6 +5,7 @@ import com.gamershall.domain.model.JogoJogado;
 import com.gamershall.domain.model.Usuario;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +13,7 @@ public class RegistroJogoJogadoService {
 
     private final RegistroUsuarioService registroUsuarioService;
 
+    @Transactional
     public JogoJogado registrar(Long usuarioId, JogoJogado jogoJogado){
         Usuario usuario = registroUsuarioService.buscar(usuarioId);
 
