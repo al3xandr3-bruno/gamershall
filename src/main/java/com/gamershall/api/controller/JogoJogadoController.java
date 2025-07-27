c5023fa912685a5ed9f636c64fd2b9eaf9715af1
al3xandr3-bruno <brunoalexdias15@gmail.com>
2025-07-26 21:53:09 -0300

    🔨 Defining a controller for an user's played games

---
+++ src/main/java/com/gamershall/api/controller/JogoJogadoController.java
@@ -0,0 +1,38 @@
+package com.gamershall.api.controller;
+
+import com.gamershall.api.mapper.JogoMapper;
+import com.gamershall.api.model.JogoModel;
+import com.gamershall.api.model.input.JogoInput;
+import com.gamershall.domain.model.JogoJogado;
+import com.gamershall.domain.repository.JogoJogadoRepository;
+import com.gamershall.domain.services.RegistroUsuarioService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/usuarios/{usuarioId}/jogos")
+public class JogoJogadoController {
+
+    private final JogoJogadoRepository jogoJogadoRepository;
+    private final RegistroUsuarioService registroUsuarioService;
+    private final JogoMapper jogoMapper;
+
+    @GetMapping("/{usuarioId}/jogos")
+    public List<JogoModel> buscarJogos(@PathVariable Long usuarioId){
+        return jogoJogadoRepository.findByUsuario(registroUsuarioService.buscar(usuarioId))
+                .stream()
+                .map(JogoJogado::getJogo)
+                .map(jogoMapper::toModel)
+                .toList();
+    }
+
+    @PostMapping("/{usuarioId}/jogos")
+    public JogoModel adicionarJogo(@RequestBody JogoInput jogoInput, @PathVariable Long usuarioId){
+        JogoJogado jogoJogado = new JogoJogado();
+        jogoJogado.setJogo(jogoMapper.toEntity(jogoInput));
+        return jogoMapper.toModel(registroUsuarioService.buscar(usuarioId).adicionaJogo(jogoJogado).getJogo());
+    }
+}
--- src/main/java/com/gamershall/api/controller/UsuarioController.java
+++ src/main/java/com/gamershall/api/controller/UsuarioController.java
@@ -3,11 +3,7 @@ package com.gamershall.api.controller;
 import com.gamershall.api.mapper.UsuarioMapper;
 import com.gamershall.api.model.UsuarioModel;
 import com.gamershall.api.model.input.UsuarioInput;
-import com.gamershall.domain.model.Jogo;
-import com.gamershall.domain.model.JogoJogado;
 import com.gamershall.domain.model.Usuario;
-import com.gamershall.domain.repository.JogoJogadoRepository;
-import com.gamershall.domain.repository.JogoRepository;
 import com.gamershall.domain.repository.UsuarioRepository;
 import com.gamershall.domain.services.RegistroUsuarioService;
 import lombok.AllArgsConstructor;
@@ -24,7 +20,6 @@ public class UsuarioController {
 
     private final UsuarioRepository usuarioRepository;
     private final RegistroUsuarioService registroUsuarioService;
-    private final JogoJogadoRepository jogoJogadoRepository;
     private final UsuarioMapper usuarioMapper;
 
     @GetMapping
@@ -38,21 +33,6 @@ public class UsuarioController {
                 .map(usuarioMapper::toModel)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
-    }
-
-    @GetMapping("/{usuarioId}/jogos")
-    public List<Jogo> buscarJogos(@PathVariable Long usuarioId){
-        return jogoJogadoRepository.findByUsuario(registroUsuarioService.buscar(usuarioId))
-                .stream()
-                .map(JogoJogado::getJogo)
-                .toList();
-    }
-
-    @PostMapping("/{usuarioId}/jogos")
-    public Jogo adicionarJogo(@RequestBody Jogo jogo, @PathVariable Long usuarioId){
-        JogoJogado jogoJogado = new JogoJogado();
-        jogoJogado.setJogo(jogo);
-        return registroUsuarioService.buscar(usuarioId).adicionaJogo(jogoJogado).getJogo();
     }
 
     @PostMapping
